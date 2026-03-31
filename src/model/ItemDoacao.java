f599a97352ce0c2c0036d00fcd895ec25551e7b4
Matheus Maynart de Araujo Barreto <matheusmaynart8@gmal.com>
2026-03-31 12:17:23 -0300

    feat: modelagem de ItemDoacao com estados de conservacao e status do sistema

---
+++ src/model/ItemDoacao.java
@@ -0,0 +1,108 @@
+package model;
+
+public class ItemDoacao  {
+    private int id;
+    private String nomeItem;
+    private String categoria;
+    private String descricao;
+    private int quantidade;
+    private String estadoConservacao;
+    private String dataCadastro;
+    private String status;
+    
+
+public ItemDoacao(int id, String nomeItem, String categoria, String descricao, int quantidade, String estadoConservacao, String dataCadastro, String status){
+    this.id = id;
+    this.nomeItem = nomeItem;
+    this.categoria = categoria;
+    this.descricao = descricao;
+    this.quantidade = quantidade;
+    this.estadoConservacao = estadoConservacao;
+    this.dataCadastro = dataCadastro; 
+    this.status = status;
+
+
+}    
+
+public void setId(int id){
+    this.id = id;
+
+}
+
+public int getId(){
+    return id;
+
+}
+
+public void setNomeItem(String nomeItem){
+    this.nomeItem = nomeItem;
+
+}
+
+public String getNomeItem(){
+    return nomeItem;
+
+}
+
+public void setCategoria(String categoria){
+    this.categoria = categoria;
+
+}
+
+public String getCatergoria(){
+    return categoria;
+
+}
+
+public void setDescricao(String descricao){
+    this.descricao = descricao;
+
+}
+
+public String descricao(){
+    return descricao;
+
+}
+
+public void setQuantidade(int quantidade){
+    this.quantidade = quantidade;
+
+}
+
+public int getQuantidade(){
+    return quantidade;
+
+}
+
+public void setEstadoConservacao(String estadoConservacao){
+    this.estadoConservacao = estadoConservacao;
+
+}
+
+public String getEstadoConservacao(){
+    return estadoConservacao;
+
+}
+
+public void setDataCadastro(String dataCadastro){
+    this.dataCadastro = dataCadastro;
+
+}
+
+public String getDataCadastro(){
+    return dataCadastro;
+
+}
+
+public void setStatus(String status){
+    this.status = status;
+
+}
+
+public String getStatus(){
+    return status;
+    
+}
+
+
+}

9c5f7323aaec0280c767e515f3ad14b9e661bb74
Matheus Maynart de Araujo Barreto <matheusmaynart8@gmal.com>
2026-03-31 12:15:44 -0300

    feat: classe Beneficiario com atributos especificos de tipo e prioridade

---
+++ src/model/Beneficiario.java
@@ -0,0 +1,34 @@
+package model;
+
+public class Beneficiario extends Usuario {
+    private String tipo;
+    private String prioridade;
+  
+    
+public Beneficiario(int id, String nome, String telefone, String email, String endereco, String tipo, String prioridade){
+    super(id, nome, telefone, email, endereco);
+    this.tipo = tipo;
+    this.prioridade = prioridade;
+} 
+
+public void setTipo(String tipo){
+    this.tipo = tipo;
+
+}
+
+public String getTipo(){
+    return tipo;
+
+}
+
+public void setPrioridade(String prioridade){
+    this.prioridade = prioridade;
+
+}
+
+public String getPrioridade(){
+    return prioridade;
+    
+}
+
+}

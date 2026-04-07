697ba3adc5ae0a2bbf2730ff0f133722e6d9c4ad
Matheus Maynart de Araujo Barreto <matheusmaynart8@gmal.com>
2026-04-07 11:54:13 -0300

    feat: modelagem da classe ItemDoacaoService com validações

---
+++ src/service/ItemDoacaoService.java
@@ -0,0 +1,37 @@
+package service;
+
+import model.ItemDoacao;
+import repository.ItemDoacaoRepository;
+
+public class ItemDoacaoService {
+    private ItemDoacaoRepository repository = new ItemDoacaoRepository();
+
+
+    public void cadastrarItem(ItemDoacao item){
+        if (item == null) {
+            System.out.println("[!] Erro: Dados do intem não foram preenchidos corretamente.");
+            return;
+        }
+            
+        if (item.getNomeItem() == null || item.getNomeItem().isEmpty()){
+                System.out.println("[!] Erro: Nome do item é obrigatorio.");
+                return;
+        }
+
+        item.setStatus("DISPONIVEL");
+        repository.salvar(item);
+
+
+
+        
+      
+            
+        
+
+    }
+}
+    
+    
+
+    
+

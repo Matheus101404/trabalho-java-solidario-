867a809185b1c8a682f85dcbb1ffc070c778a941
Matheus Maynart de Araujo Barreto <matheusmaynart8@gmal.com>
2026-04-14 11:56:03 -0300

    feat: modelagem da classe ItemDoacao com constantes de categoria e status

--- src/model/ItemDoacao.java
+++ src/model/ItemDoacao.java
@@ -9,100 +9,108 @@ public class ItemDoacao  {
     private String estadoConservacao;
     private String dataCadastro;
     private String status;
+    public static final String STATUS_DISPONIVEL = "DISPONIVEL";
+    public static final String STATUS_RESERVADO = "RESERVADO";
+    public static final String STATUS_ENTREGUE = "ENTREGUE";
+    public static final String STATUS_CANCELADO = "CANCELADO";
+    public static final String CATEGORIA_ALIMENTO = "ALIMENTO";
+    public static final String CATEGORIA_ROUPA = "ROUPA";
+    public static final String CATEGORIA_HIGIENE = "HIGIENE";
+    public static final String CATEGORIA_MOVEIS = "MOVEIS";
     
 
-public ItemDoacao(int id, String nomeItem, String categoria, String descricao, int quantidade, String estadoConservacao, String dataCadastro, String status){
-    this.id = id;
-    this.nomeItem = nomeItem;
-    this.categoria = categoria;
-    this.descricao = descricao;
-    this.quantidade = quantidade;
-    this.estadoConservacao = estadoConservacao;
-    this.dataCadastro = dataCadastro; 
-    this.status = status;
+    public ItemDoacao(int id, String nomeItem, String categoria, String descricao, int quantidade, String estadoConservacao, String dataCadastro, String status){
+        this.id = id;
+        this.nomeItem = nomeItem;
+        this.categoria = categoria;
+        this.descricao = descricao;
+        this.quantidade = quantidade;
+        this.estadoConservacao = estadoConservacao;
+        this.dataCadastro = dataCadastro; 
+        this.status = status;
 
 
-}    
+    }    
 
-public void setId(int id){
-    this.id = id;
+    public void setId(int id){
+        this.id = id;
 
-}
+    }
 
-public int getId(){
-    return id;
+    public int getId(){
+        return id;
 
-}
+    }
 
-public void setNomeItem(String nomeItem){
-    this.nomeItem = nomeItem;
+    public void setNomeItem(String nomeItem){
+        this.nomeItem = nomeItem;
 
-}
+    }
 
-public String getNomeItem(){
-    return nomeItem;
+    public String getNomeItem(){
+        return nomeItem;
 
-}
+    }
 
-public void setCategoria(String categoria){
-    this.categoria = categoria;
+    public void setCategoria(String categoria){
+        this.categoria = categoria;
 
-}
+    }
 
-public String getCatergoria(){
-    return categoria;
+    public String getCategoria(){
+        return categoria;
 
-}
+    }
 
-public void setDescricao(String descricao){
-    this.descricao = descricao;
+    public void setDescricao(String descricao){
+        this.descricao = descricao;
 
-}
+    }
 
-public String descricao(){
-    return descricao;
+    public String descricao(){
+        return descricao;
 
-}
+    }
 
-public void setQuantidade(int quantidade){
-    this.quantidade = quantidade;
+    public void setQuantidade(int quantidade){
+        this.quantidade = quantidade;
 
-}
+    }
 
-public int getQuantidade(){
-    return quantidade;
+    public int getQuantidade(){
+        return quantidade;
 
-}
+    }
 
-public void setEstadoConservacao(String estadoConservacao){
-    this.estadoConservacao = estadoConservacao;
+    public void setEstadoConservacao(String estadoConservacao){
+        this.estadoConservacao = estadoConservacao;
 
-}
+    }
 
-public String getEstadoConservacao(){
-    return estadoConservacao;
+    public String getEstadoConservacao(){
+        return estadoConservacao;
 
-}
+    }
 
-public void setDataCadastro(String dataCadastro){
-    this.dataCadastro = dataCadastro;
+    public void setDataCadastro(String dataCadastro){
+        this.dataCadastro = dataCadastro;
 
-}
+    }
 
-public String getDataCadastro(){
-    return dataCadastro;
+    public String getDataCadastro(){
+        return dataCadastro;
 
-}
+    }
 
-public void setStatus(String status){
-    this.status = status;
+    public void setStatus(String status){
+        this.status = status;
 
-}
+    }
 
-public String getStatus(){
-    return status;
-    
-}
+    public String getStatus(){
+        return status;
+        
+    }
 
 
 }

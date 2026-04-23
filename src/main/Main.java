16aa4eb6f013399b102b08bd76d026cf9fb0a860
Matheus Maynart <matheusmaynart8@gmail.com>
2026-04-23 16:52:40 -0300

    feat: modelagem para cadastro e listagem de Doaodor e Beneficiario

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,19 +1,31 @@
 package main;
 import java.util.Scanner;
+
+import model.Beneficiario;
+import model.Doador;
 import model.ItemDoacao;
+import service.BeneficiarioService;
+import service.DoadorService;
 import service.ItemDoacaoService;
 
 public class Main {
     public static void main(String[] args){
         Scanner leitor = new Scanner(System.in);
         ItemDoacaoService itemService = new ItemDoacaoService();
+        DoadorService doadorService = new DoadorService();
+        BeneficiarioService beneficiarioService = new BeneficiarioService();
         int opcao = -1;
 
         System.out.println("=== SISTEMA DE DOAÇÃO SOLIDÁRIA - CHECKPOINT 1 ===");
 
         do {
+            System.out.println();
             System.out.println(" 1 - Cadastrar Item para Doação");
             System.out.println(" 2 - Listar Itens");
+            System.out.println(" 3 - Cadastrar Doador");
+            System.out.println(" 4 - Listar Doadores");
+            System.out.println(" 5 - Cadastrar Beneficiario");
+            System.out.println(" 6 - Listar Beneficiorios ");
             System.out.println(" 0 - Sair");
             System.out.print("Opção: ");
 
@@ -31,6 +43,27 @@ public class Main {
                     itemService.listarTodos().forEach(item ->  
                         System.out.println(item.getNomeItem() + " | Status: " + item.getStatus()));
                     break;
+
+                case 3:
+                    cadastrarNovoDoador(leitor, doadorService);
+                    break;
+                    
+                case 4:
+                    System.out.println("--- DOADORES CADASTRADOS ---");
+                    doadorService.listarTodos().forEach(doador ->
+                        System.out.println(doador.getNome()));
+                    break;
+                 
+                case 5:
+                     cadastrarNovoBeneficiario(leitor, beneficiarioService);
+                     break;
+
+                case 6:
+                    System.out.println("--- BENEFICIARIOS CADASTRADOS ---");
+                    beneficiarioService.listarTodos().forEach(beneficiario ->
+                        System.out.println(beneficiario.getNome()));
+                    break;     
+
 
                 case 0:
                     System.out.println("Encerrando...");
@@ -66,13 +99,75 @@ public class Main {
 
         System.out.println("Status: ");
         String status = leitor.nextLine();
-        
-
 
         ItemDoacao novo = new ItemDoacao(1, nome, categoria, descricao, quantidade, estadoConservacao, dataCadastro, status);
 
         service.cadastrarItem(novo);
         System.out.println("Item cadastrado com sucesso!");
+
+
+    }
+
+            
+    private static void cadastrarNovoDoador(Scanner leitor, DoadorService service){
+        System.out.println("--- CADASTRO DO DOADOR ---");
+
+        System.out.println("CPF do Doador: ");
+        int id = leitor.nextInt();
+        leitor.nextLine();
+
+        System.out.println("Nome do Doador: ");
+        String nome = leitor.nextLine();
+
+        System.out.println("Telefone do Doador: ");
+        String telefone = leitor.nextLine();
+
+        System.out.println("Email do Doador: ");
+        String email = leitor.nextLine();
+
+        System.out.println("Endereço do Doador: ");
+        String endereco = leitor.nextLine();
+
+        Doador novo = new Doador(id, nome, telefone, email, endereco);
+        service.cadastrarDoador(novo);
+        System.out.println("Doador cadastrado com sucesso!");
+
+
+    }    
+
+
+    private static void cadastrarNovoBeneficiario(Scanner leitor, BeneficiarioService service){
+        System.out.println("CPF do Beneficiario: ");
+        int id = leitor.nextInt();
+        leitor.nextLine();
+
+        System.out.println("Nome do Beneficiario: ");
+        String nome = leitor.nextLine();
+
+        System.out.println("Telefone do Beneficiareio: ");
+        String telefone = leitor.nextLine();
+
+        System.out.println("Email do Beneficiario: ");
+        String email = leitor.nextLine();
+
+        System.out.println("Endereço do Beneficiario: ");
+        String endereco = leitor.nextLine();
+
+        System.out.println("Tipo de Beneficiario: ");
+        String tipo = leitor.nextLine();
+
+        System.out.println("Prioridade do Beneficiario: ");
+        String prioridade = leitor.nextLine();
+
+        Beneficiario novo = new Beneficiario(id, nome, telefone, email, endereco, tipo, prioridade);
+        service.cadastrarBeneficiario(novo);
+        System.out.println("Beneficiario cadastrado com sucesso!");
+
+
+    }    
+
+
+        
     }
     
-}
+

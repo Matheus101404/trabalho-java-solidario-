afe7e935ffb7c25b32aaed477f9be6b3212fd21f
Matheus Maynart de Araujo Barreto <matheusmaynart8@gmal.com>
2026-05-21 11:54:03 -0300

    fix: correção de tipo de variavel

--- src/model/Doador.java
+++ src/model/Doador.java
@@ -2,7 +2,7 @@ package model;
 
 public class Doador extends Usuario {
 
-    public Doador(int id, String nome, String telefone, String email, String endereco){
+    public Doador(String id, String nome, String telefone, String email, String endereco){
         super(id, nome, telefone, email, endereco);
 
     }

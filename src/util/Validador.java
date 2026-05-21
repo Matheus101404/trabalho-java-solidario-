b9144e7a3d34f062206c33257e78d51ba374c61a
Matheus Maynart de Araujo Barreto <matheusmaynart8@gmal.com>
2026-05-21 11:55:15 -0300

    feat: criação de metodos de tratamento de erros

---
+++ src/util/Validador.java
@@ -0,0 +1,39 @@
+package util;
+
+import java.util.Scanner;
+
+public class Validador {
+
+    public static String lerCPF(Scanner leitor) {
+        while (true) {
+            String cpf = leitor.nextLine().trim();
+            cpf = cpf.replaceAll("[^0-9]", ""); // aqui eu chamei o metodo "replace" para que ele delete tudo que não for numeros de 0 a 9
+                                            
+
+            if (cpf.length() == 11) {
+                return cpf;
+
+            } else {
+                System.out.println("Erro: O CPF deve conter exatamente 11 números! Tente novamente.");
+            }
+
+        }
+    }
+
+    public static String lerTelefone(Scanner leitor) {
+        while (true) {
+
+            String telefone = leitor.nextLine().trim();
+            telefone = telefone.replaceAll("[^0-9]", "");
+
+            if (telefone.length() == 11) {
+                return telefone;
+
+            } else {
+                System.out.println("Erro: O telefone deve conter 11 números (xx)xxx...! Tente novamente.");
+
+            }
+
+        }
+    }
+}
